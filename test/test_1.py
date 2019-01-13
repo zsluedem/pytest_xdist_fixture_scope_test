4b72ec374c1912d78ea18c43b302d92f7327898a
will <zsluedem06@gmail.com>
2019-01-13 13:32:27 +0800

    first

---
+++ test/test_1.py
@@ -0,0 +1,40 @@
+#
+# MIT License
+#
+# Copyright (c) 2018 WillQ
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+import logging
+import pytest
+logger = logging.getLogger(__file__)
+
+def test_func_object(func_object, module_object, session_object):
+    assert False
+
+def test_func_object_2(func_object, module_object, session_object):
+    assert False
+
+def test_func_object_3(func_object, module_object, session_object):
+    assert False
+# @pytest.mark.trylast
+# def test_func_object_4(func_object, module_object):
+#     logger.info(module_object.o1)
+#     logger.info(module_object.o2)
+#     logger.info(module_object.o3)
